0527bca13c593a2cf2431223991db78fa099e524
Balaji <125738570+BalajiGitGeek@users.noreply.github.com>
2023-03-07 18:13:23 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+// Footer component for footer section
+const Footer = () => {
+  return (
+    <div className="footer">
+      Created By
+      <i className="fa-solid fa-heart"></i>
+      <a href="https://www.linkedin.com/in/balajiguru/" target="_blank" title="Balaji's Linkedin Profile">
+        Balaji
+      </a>
+      <i className="fa-solid fa-copyright"></i>2023
+      <strong>
+        Tick<span>Food</span>
+      </strong>
+    </div>
+  );
+};
+
+export default Footer;
